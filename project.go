544d5fc0ca7f8cac36f0281bd8020c3e1b42210e
nscuro <nscuro@protonmail.com>
2021-08-29 00:00:00 +0200

    cud operations for project
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- project.go
+++ project.go
@@ -9,22 +9,22 @@ import (
 )
 
 type Project struct {
-	UUID        uuid.UUID `json:"uuid"`
-	Author      string    `json:"author"`
-	Publisher   string    `json:"publisher"`
-	Group       string    `json:"group"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Version     string    `json:"version"`
-	Classifier  string    `json:"classifier"`
+	UUID        uuid.UUID `json:"uuid,omitempty"`
+	Author      string    `json:"author,omitempty"`
+	Publisher   string    `json:"publisher,omitempty"`
+	Group       string    `json:"group,omitempty"`
+	Name        string    `json:"name,omitempty"`
+	Description string    `json:"description,omitempty"`
+	Version     string    `json:"version,omitempty"`
+	Classifier  string    `json:"classifier,omitempty"`
 
-	CPE       string `json:"cpe"`
-	PURL      string `json:"purl"`
-	SWIDTagID string `json:"swidTagId"`
+	CPE       string `json:"cpe,omitempty"`
+	PURL      string `json:"purl,omitempty"`
+	SWIDTagID string `json:"swidTagId,omitempty"`
 
-	DirectDependencies string            `json:"directDependencies"`
-	Properties         []ProjectProperty `json:"properties"`
-	Tags               []Tag             `json:"tags"`
+	DirectDependencies string            `json:"directDependencies,omitempty"`
+	Properties         []ProjectProperty `json:"properties,omitempty"`
+	Tags               []Tag             `json:"tags,omitempty"`
 	Active             bool              `json:"active"`
 }
 
@@ -39,26 +39,6 @@ type ProjectService struct {
 
 func (p ProjectService) Get(ctx context.Context, projectUUID uuid.UUID) (*Project, error) {
 	req, err := p.client.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/project/%s", projectUUID))
-	if err != nil {
-		return nil, err
-	}
-
-	var project Project
-	_, err = p.client.doRequest(req, &project)
-	if err != nil {
-		return nil, err
-	}
-
-	return &project, nil
-}
-
-func (p ProjectService) Lookup(ctx context.Context, name, version string) (*Project, error) {
-	params := map[string]string{
-		"name":    name,
-		"version": version,
-	}
-
-	req, err := p.client.newRequest(ctx, http.MethodGet, "/api/v1/project/lookup", withParams(params))
 	if err != nil {
 		return nil, err
 	}
@@ -88,6 +68,70 @@ func (p ProjectService) GetAll(ctx context.Context, po PageOptions) (*ProjectsPa
 		TotalCount: res.TotalCount,
 		Projects:   projects,
 	}, nil
+}
+
+func (p ProjectService) Create(ctx context.Context, project Project) (*Project, error) {
+	req, err := p.client.newRequest(ctx, http.MethodPut, "/api/v1/project", withBody(project))
+	if err != nil {
+		return nil, err
+	}
+
+	var createdProject Project
+	_, err = p.client.doRequest(req, &createdProject)
+	if err != nil {
+		return nil, err
+	}
+
+	return &createdProject, nil
+}
+
+func (p ProjectService) Update(ctx context.Context, project Project) (*Project, error) {
+	req, err := p.client.newRequest(ctx, http.MethodPost, "/api/v1/project", withBody(project))
+	if err != nil {
+		return nil, err
+	}
+
+	var createdProject Project
+	_, err = p.client.doRequest(req, &createdProject)
+	if err != nil {
+		return nil, err
+	}
+
+	return &createdProject, nil
+}
+
+func (p ProjectService) Delete(ctx context.Context, projectUUID uuid.UUID) error {
+	req, err := p.client.newRequest(ctx, http.MethodDelete, fmt.Sprintf("/api/v1/project/%s", projectUUID))
+	if err != nil {
+		return err
+	}
+
+	_, err = p.client.doRequest(req, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p ProjectService) Lookup(ctx context.Context, name, version string) (*Project, error) {
+	params := map[string]string{
+		"name":    name,
+		"version": version,
+	}
+
+	req, err := p.client.newRequest(ctx, http.MethodGet, "/api/v1/project/lookup", withParams(params))
+	if err != nil {
+		return nil, err
+	}
+
+	var project Project
+	_, err = p.client.doRequest(req, &project)
+	if err != nil {
+		return nil, err
+	}
+
+	return &project, nil
 }
 
 type ProjectCloneRequest struct {
