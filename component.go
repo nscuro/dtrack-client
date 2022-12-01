01bd823c0edbbaa0e2488df04a5944cac6ce2109
spyros <northdpole@users.noreply.github.com>
2022-12-01 13:55:23 +0000

    add component create method

--- component.go
+++ component.go
@@ -9,37 +9,37 @@ import (
 )
 
 type Component struct {
-	UUID               uuid.UUID `json:"uuid"`
-	Author             string    `json:"author"`
-	Publisher          string    `json:"publisher"`
-	Group              string    `json:"group"`
+	UUID               uuid.UUID `json:"uuid,omitempty"`
+	Author             string    `json:"author,omitempty"`
+	Publisher          string    `json:"publisher,omitempty"`
+	Group              string    `json:"group,omitempty"`
 	Name               string    `json:"name"`
 	Version            string    `json:"version"`
-	Classifier         string    `json:"classifier"`
-	FileName           string    `json:"filename"`
-	Extension          string    `json:"extension"`
-	MD5                string    `json:"md5"`
-	SHA1               string    `json:"sha1"`
-	SHA256             string    `json:"sha256"`
-	SHA384             string    `json:"sha384"`
-	SHA512             string    `json:"sha512"`
-	SHA3_256           string    `json:"sha3_256"`
-	SHA3_384           string    `json:"sha3_384"`
-	SHA3_512           string    `json:"sha3_512"`
-	BLAKE2b_256        string    `json:"blake2b_256"`
-	BLAKE2b_384        string    `json:"blake2b_384"`
-	BLAKE2b_512        string    `json:"blake2b_512"`
-	BLAKE3             string    `json:"blake3"`
-	CPE                string    `json:"cpe"`
-	PURL               string    `json:"purl"`
-	SWIDTagID          string    `json:"swidTagId"`
-	Internal           bool      `json:"isInternal"`
-	Description        string    `json:"description"`
-	Copyright          string    `json:"copyright"`
-	License            string    `json:"license"`
-	ResolvedLicense    *License  `json:"resolvedLicense"`
-	DirectDependencies string    `json:"directDependencies"`
-	Notes              string    `json:"notes"`
+	Classifier         string    `json:"classifier,omitempty"`
+	FileName           string    `json:"filename,omitempty"`
+	Extension          string    `json:"extension,omitempty"`
+	MD5                string    `json:"md5,omitempty"`
+	SHA1               string    `json:"sha1,omitempty"`
+	SHA256             string    `json:"sha256,omitempty"`
+	SHA384             string    `json:"sha384,omitempty"`
+	SHA512             string    `json:"sha512,omitempty"`
+	SHA3_256           string    `json:"sha3_256,omitempty"`
+	SHA3_384           string    `json:"sha3_384,omitempty"`
+	SHA3_512           string    `json:"sha3_512,omitempty"`
+	BLAKE2b_256        string    `json:"blake2b_256,omitempty"`
+	BLAKE2b_384        string    `json:"blake2b_384,omitempty"`
+	BLAKE2b_512        string    `json:"blake2b_512,omitempty"`
+	BLAKE3             string    `json:"blake3,omitempty"`
+	CPE                string    `json:"cpe,omitempty"`
+	PURL               string    `json:"purl,omitempty"`
+	SWIDTagID          string    `json:"swidTagId,omitempty"`
+	Internal           bool      `json:"isInternal,omitempty"`
+	Description        string    `json:"description,omitempty"`
+	Copyright          string    `json:"copyright,omitempty"`
+	License            string    `json:"license,omitempty"`
+	ResolvedLicense    *License  `json:"resolvedLicense,omitempty"`
+	DirectDependencies string    `json:"directDependencies,omitempty"`
+	Notes              string    `json:"notes,omitempty"`
 }
 
 type ComponentService struct {
@@ -68,5 +68,20 @@ func (cs ComponentService) GetAll(ctx context.Context, projectUUID uuid.UUID, po
 	}
 
 	p.TotalCount = res.TotalCount
+	return
+}
+
+func (cs ComponentService) Create(ctx context.Context, projectUUID string, component Component) (c Component, err error) {
+	if err != nil {
+		return
+	}
+	req, err := cs.client.newRequest(ctx, http.MethodPut,
+		fmt.Sprintf("/api/v1/component/project/%s", projectUUID),
+		withBody(component))
+	if err != nil {
+		return
+	}
+
+	_, err = cs.client.doRequest(req, &c)
 	return
 }
